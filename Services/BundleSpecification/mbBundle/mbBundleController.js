3a119842f7bd16f7ffd1bf3d3db2964aa35cf0cf
dking1234 <39795989+dking1234@users.noreply.github.com>
2023-08-17 16:05:26 +0800

    Update mbBundleController.js

--- Services/BundleSpecification/mbBundle/mbBundleController.js
+++ Services/BundleSpecification/mbBundle/mbBundleController.js
@@ -1,33 +1,33 @@
-// controllers/mbBundleController.js
-const express = require('express');
-const mbBundle = require('../mbBundle/mbBundleModel');
+    // controllers/mbBundleController.js
+    const express = require('express');
+    const mbBundle = require('../mbBundle/mbBundleModel');
 
-const router = express.Router();
+    const router = express.Router();
 
-// Create a new mbBundle
-router.post('/mbBundles', async (req, res) => {
-  const { name, unitPricePerMB } = req.body;
+    // Create a new mbBundle
+    router.post('/mbBundles', async (req, res) => {
+    const { name, unitPricePerMB } = req.body;
 
-  try {
-    const newMbBundle = new mbBundle({ name, unitPricePerMB });
-    await newMbBundle.save();
+    try {
+        const newMbBundle = new mbBundle({ name, unitPricePerMB });
+        await newMbBundle.save();
 
-    res.status(201).json(newMbBundle);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+        res.status(201).json(newMbBundle);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    });
 
-// Calculate the amount of MB based on the unit price
-router.post('/mbBundles/calculate', async (req, res) => {
-  const { amount, unitPricePerMB } = req.body;
+    // Calculate the amount of MB based on the unit price
+    router.post('/mbBundles/calculate', async (req, res) => {
+    const { amount, unitPricePerMB } = req.body;
 
-  try {
-    const mbAmount = amount / unitPricePerMB;
-    res.status(200).json({ mbAmount });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+    try {
+        const mbAmount = amount / unitPricePerMB;
+        res.status(200).json({ mbAmount });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+    });
 
-module.exports = router;
+    module.exports = router;
